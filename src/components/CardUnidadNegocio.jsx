607f6f95cf99ef5e6125c8d8a12451eedc5e5843
JULIAN DARIO GONZALEZ TOLEDO <julian.gonzalez@hokma.ai>
2025-09-15 12:01:42 -0500

    fix: Se ajusta el el estilo de la card al del diseño

--- src/components/CardUnidadNegocio.jsx
+++ src/components/CardUnidadNegocio.jsx
@@ -3,25 +3,32 @@ import IconoHokma from './IconoHokma'
 const CardUnidadNegocio = ({ 
   color = "#ffffff", 
   imagen, 
-  backgroundColor = "#292C3A" 
+  backgroundColor = "#292C3A",
+  isSelected = false,
+  onSelect
 }) => {
   return (
     <div 
-      className="rounded-3xl px-6 shadow-lg transform transition-transform duration-300 hover:scale-105 cursor-pointer"
+      className={`rounded-3xl px-6 shadow-lg transition-all duration-300 cursor-pointer ${
+        isSelected 
+          ? 'scale-105' 
+          : 'transform hover:scale-105'
+      }`}
       style={{ backgroundColor }}
+      onClick={onSelect}
     >
-      <div className="grid grid-cols-[1fr_3fr] items-center">
+      <div className="grid py-2 grid-cols-[1fr_3fr] items-center gap-5">
         {/* Primera columna - Icono Hokma */}
         <div className="flex justify-center">
-          <IconoHokma size={60} color={color} />
+          <IconoHokma size={40} color={color} />
         </div>
         
         {/* Segunda columna - Imagen */}
-        <div className="flex justify-center">
+        <div className="flex justify-center w-48">
           <img 
             src={imagen} 
             alt="Unidad de Negocio"
-            className="w-64 h-64 object-contain"
+            className="w-full h-32 object-cover"
           />
         </div>
       </div>
