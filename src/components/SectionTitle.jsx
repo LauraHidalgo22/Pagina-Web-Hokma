616c7c263ac0abe7e89db77bb7449a8354384bdb
JULIAN DARIO GONZALEZ TOLEDO <julian.gonzalez@hokma.ai>
2025-09-15 11:07:38 -0500

    fix: Se agrega propiedad titleClassName al componente SectionTitle

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -3,13 +3,14 @@ import { } from 'react'
 const SectionTitle = ({ 
   children, 
   className = "",
+  titleClassName = "",
   ...props 
 }) => {
   return (
     <div className={`relative flex items-start justify-start ${className}`} {...props}>
       {/* Título principal */}
       <h2 
-        className="relative z-10 text-left font-bold text-gray-800"
+        className={`relative z-10 text-left font-bold text-gray-800 ${titleClassName}`}
         style={{
           fontFamily: 'Caviar Dreams, sans-serif',
           fontSize: '40pt',
